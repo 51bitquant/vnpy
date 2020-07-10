d6bf05238302b9bcd2cde5eacf3773fac6ff25e9
lin <lin>
2020-07-10 18:23:04 +0800

    remove stock exchange.

--- examples/vn_trader/run.py
+++ examples/vn_trader/run.py
@@ -6,38 +6,20 @@ from vnpy.trader.ui import MainWindow, create_qapp
 
 # from vnpy.gateway.binance import BinanceGateway
 # from vnpy.gateway.bitmex import BitmexGateway
-# from vnpy.gateway.futu import FutuGateway
-# from vnpy.gateway.ib import IbGateway
-# from vnpy.gateway.ctp import CtpGateway
-# from vnpy.gateway.ctptest import CtptestGateway
-# from vnpy.gateway.mini import MiniGateway
-# from vnpy.gateway.sopt import SoptGateway
-# from vnpy.gateway.minitest import MinitestGateway
-# from vnpy.gateway.femas import FemasGateway
-# from vnpy.gateway.tiger import TigerGateway
-# from vnpy.gateway.oes import OesGateway
 # from vnpy.gateway.okex import OkexGateway
 # from vnpy.gateway.huobi import HuobiGateway
 # from vnpy.gateway.bitfinex import BitfinexGateway
 # from vnpy.gateway.onetoken import OnetokenGateway
 # from vnpy.gateway.okexf import OkexfGateway
 # from vnpy.gateway.okexs import OkexsGateway
-# from vnpy.gateway.xtp import XtpGateway
 # from vnpy.gateway.huobif import HuobifGateway
-# from vnpy.gateway.tap import TapGateway
-# from vnpy.gateway.tora import ToraGateway
-# from vnpy.gateway.alpaca import AlpacaGateway
-# from vnpy.gateway.da import DaGateway
 # from vnpy.gateway.coinbase import CoinbaseGateway
 # from vnpy.gateway.bitstamp import BitstampGateway
 # from vnpy.gateway.gateios import GateiosGateway
 # from vnpy.gateway.bybit import BybitGateway
 # from vnpy.gateway.deribit import DeribitGateway
-# from vnpy.gateway.uft import UftGateway
 # from vnpy.gateway.okexo import OkexoGateway
 # from vnpy.gateway.binancef import BinancefGateway
-# from vnpy.gateway.mt4 import Mt4Gateway
-from vnpy.gateway.mt5 import Mt5Gateway
 
 # from vnpy.app.cta_strategy import CtaStrategyApp
 # from vnpy.app.csv_loader import CsvLoaderApp
@@ -65,30 +47,15 @@ def main():
     main_engine = MainEngine(event_engine)
 
     # main_engine.add_gateway(BinanceGateway)
-    # main_engine.add_gateway(CtpGateway)
-    # main_engine.add_gateway(CtptestGateway)
-    # main_engine.add_gateway(MiniGateway)
-    # main_engine.add_gateway(SoptGateway)
-    # main_engine.add_gateway(MinitestGateway)
-    # main_engine.add_gateway(FemasGateway)
-    # main_engine.add_gateway(UftGateway)
-    # main_engine.add_gateway(IbGateway)
-    # main_engine.add_gateway(FutuGateway)
     # main_engine.add_gateway(BitmexGateway)
-    # main_engine.add_gateway(TigerGateway)
-    # main_engine.add_gateway(OesGateway)
     # main_engine.add_gateway(OkexGateway)
     # main_engine.add_gateway(HuobiGateway)
     # main_engine.add_gateway(BitfinexGateway)
     # main_engine.add_gateway(OnetokenGateway)
     # main_engine.add_gateway(OkexfGateway)
     # main_engine.add_gateway(HuobifGateway)
-    # main_engine.add_gateway(XtpGateway)
-    # main_engine.add_gateway(TapGateway)
-    # main_engine.add_gateway(ToraGateway)
-    # main_engine.add_gateway(AlpacaGateway)
     # main_engine.add_gateway(OkexsGateway)
-    # main_engine.add_gateway(DaGateway)
+
     # main_engine.add_gateway(CoinbaseGateway)
     # main_engine.add_gateway(BitstampGateway)
     # main_engine.add_gateway(GateiosGateway)
@@ -96,8 +63,6 @@ def main():
     # main_engine.add_gateway(DeribitGateway)
     # main_engine.add_gateway(OkexoGateway)
     # main_engine.add_gateway(BinancefGateway)
-    # main_engine.add_gateway(Mt4Gateway)
-    main_engine.add_gateway(Mt5Gateway)
 
     # main_engine.add_app(CtaStrategyApp)
     # main_engine.add_app(CtaBacktesterApp)
